adc3a1d4fded6bab26008298edb315127e6fc0cb
ginocrusejr <famback17@gmail.com>
2019-08-24 11:23:55 -0700

    Added a button, and a text entry box. Also, after reading more documentation, I changed the way I imported tkinter!

--- weather_gui.py
+++ weather_gui.py
@@ -1,7 +1,16 @@
-from tkinter import *
-root = Tk()
+import tkinter as tk
+
+root = tk.Tk()
 root.title("G's Weather App")
-root.geometry('400x500')
+canvas = tk.Canvas(root, height='700', width='800')
+canvas.pack()
+frame = tk.Frame(root, bg='#204a63')
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+entry = tk.Entry(frame, font=50)
+entry.place(relwidth=0.65, relheight=1)
+button = tk.Button(frame, text='This should work', bg='yellow')
+button.place(relx=0.7, rely=0, relwidth=0.25, relheight=0.25)
+
 root.mainloop()
 
 
