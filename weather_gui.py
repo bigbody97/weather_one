69a2f2a620572708b6f8302bacebc4fc89e26b21
ginocrusejr <famback17@gmail.com>
2019-08-24 12:41:13 -0700

    Added a button, and a text entry box. Also, after reading more documentation, I changed the way I imported tkinter!

--- weather_gui.py
+++ weather_gui.py
@@ -1,15 +1,20 @@
 import tkinter as tk
-
+vheight = 700
+vwidth = 800
 root = tk.Tk()
 root.title("G's Weather App")
-canvas = tk.Canvas(root, height='700', width='800')
+canvas = tk.Canvas(root, height=vheight, width=vwidth)
 canvas.pack()
-frame = tk.Frame(root, bg='#204a63')
+frame = tk.Frame(root, bg='#204a63', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
-entry = tk.Entry(frame, font=50)
+entry = tk.Entry(frame, font=40)
 entry.place(relwidth=0.65, relheight=1)
-button = tk.Button(frame, text='This should work', bg='yellow')
-button.place(relx=0.7, rely=0, relwidth=0.25, relheight=0.25)
+button = tk.Button(frame, text='This should work', bg='yellow', font=40)
+button.place(relx=0.7, relwidth=0.3, relheight=1)
+lower_frame = tk.Frame(root, bg='#204a63', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+label = tk.Label(lower_frame, text='This is my label', bg='green',)
+label.place(relx=0.3, rely=0, relwidth=0.45, relheight=0.25)
 
 root.mainloop()
 
